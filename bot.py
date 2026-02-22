80ff43f5e3ae1bc174b31267f96acda087a7ae7d
matyden <imatydeni@gmail.com>
2026-02-22 18:47:22 +0200

    added context history (for every user separately) & cmd /reset (clean context history)

--- bot.py
+++ bot.py
@@ -7,12 +7,16 @@ from aiogram.filters.command import Command
 from aiogram.enums import ParseMode
 from config_reader import config
 
+# AI related settings and variables
 client = genai.Client(api_key=config.ai_token.get_secret_value())
 AI_model = "gemini-3-flash-preview"
+user_contexts = {}
+MAX_HISTORY = 30
+
+# Bot related settings and variables
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.bot_token.get_secret_value(), default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN_V2))
 dp = Dispatcher()
-
 ALLOWED_USERS = config.allowed_users
 
 def is_allowed_user(message: types.Message) -> bool:
@@ -36,16 +40,46 @@ async def about(message: types.Message):
                          "Разработчик всея Бота - @xMatyDen\n"
                          "Бот готов к работе, просто отправь сообщение!", parse_mode=None)
 
+
+@dp.message(Command("reset"))
+async def reset_context(message: types.Message):
+    user_id = message.from_user.id
+    user_contexts[user_id] = []
+    await message.answer("🔄 Контекст очищен")
+
+
 @dp.message(F.text)
 async def get_message(message: types.Message):
     if not is_allowed_user(message):
         return await message.answer("⛔ Access denied")
+
+    user_id = message.from_user.id
     content = message.text
+
+    if user_id not in user_contexts:
+        user_contexts[user_id] = []
+
+    if len(user_contexts[user_id]) > MAX_HISTORY * 2:
+        user_contexts[user_id] = user_contexts[user_id][-MAX_HISTORY*2:]
+        logging.log(logging.INFO, f"Context window was cleaned for user {user_id}")
+
+    user_contexts[user_id].append({"role": "user", "parts": [content]})
+
+
+    await bot.send_chat_action(message.chat.id, "typing")
+
     responses = client.models.generate_content(
         model = AI_model,
-        contents=content
+        contents=str(user_contexts[user_id])
     )
+
+    user_contexts[user_id].append({"role": "model", "parts": [responses.text]})
+
+    logging.log(logging.INFO, f"Current user-s context window: {user_contexts[user_id]}")
+
     await message.answer(responses.text, parse_mode=None)
+
+    print(user_contexts)
 
 
 async def main():
